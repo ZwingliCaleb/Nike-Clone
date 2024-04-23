f2c2362755f3260442730983e86181bb36d639fb
ZwingliCaleb <calebzwingli@gmail.com>
2024-04-23 17:28:52 +0300

    bug fixes

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,15 +1,27 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants";
+
 const CustomerReviews = () => {
   return (
     <section className="max-container ">
       <h3 className="font-palanquin text-center font-bold text-4xl">
         What Our <span className="text-coral-red">Customers</span> Say?
       </h3>
-      <p className="text-center text-slate-gray font-montserrat text-xl">
+      <p className="text-center info-text max-w-lg m-auto mt-4">
         Hear genuine stories from our satisfied customers about
         their exceptional experiences with us.
       </p>
-      <div className="text-center items-center">
-        Morich Brown
+      <div className="text-center items-centermt-24 flex 
+      flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+           key={review.customerName}
+           imgURL={review.imgURL}
+           customerName={review.customerName}
+           rating={review.rating}
+           feedback={review.feedback}
+           />
+        ))}
       </div>
     </section>
   )
