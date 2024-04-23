13d57ae9c3e0a08ae0059f845de6d36d95030f44
ZwingliCaleb <calebzwingli@gmail.com>
2024-04-23 13:01:46 +0300

    Completed the PopularProducts section. Building out the SuperQuality section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,4 +1,5 @@
 import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
 
 const PopularProducts = () => {
   return (
@@ -8,13 +9,22 @@ const PopularProducts = () => {
         <div className="flex flex-col justify-start
         gap-5"
         >
-          <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
-          <p className="text-slate-gray max-w-lg mt-2 font-montserrat">Experience top-notch quality and style with 
-            our sought-after selections. Discover a world of comfort
-            , design, and value
+          <h2 className="text-4xl font-palanquin
+           font-bold">Our <span className="text-coral-red">Popular</span> Products
+          </h2>
+          <p className="text-slate-gray max-w-lg mt-2
+           font-montserrat">Experience top-notch quality
+           and style with our sought-after selections.
+            Discover a world of comfort, design, and value
           </p>
         </div>
-        <div className="">
+        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 
+        grid-cols-1 sm:gap-6 gap-14"
+        >
+          {products.map((product) => (
+            <PopularProductCard key = 
+            {product.name} {...product} />
+          ))}
 
         </div>
     </section>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,26 @@
+import button from "../components/Button"
+
 const SuperQuality = () => {
   return (
-    <div>
-      SuperQuality
-    </div>
+    <section id="about-us" className="flex justify-between
+     items-center max-lg:flex-col gap-10 w-full
+      max-container"
+      >
+        <div className="flex flex-1 flex-col">
+          <h2>We Provide You Super Quality Shoes</h2>
+          <p>Ensuring premium comfort and style, our meticulously 
+            crafted footwear is designed to elevate your experience, 
+            providing you with unmatched quality, innovation, and
+             a touch of elegance.
+          </p>
+          <p>
+            Our dedication to detail and excellence ensures your satisfaction.
+          </p>
+          <button>
+            View details
+          </button>
+        </div>
+    </section>
   )
 }
 
