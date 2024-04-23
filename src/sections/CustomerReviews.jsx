bff92994b3f4877116e358198eaa68e9c4015feb
ZwingliCaleb <calebzwingli@gmail.com>
2024-04-23 15:37:57 +0300

    create a customers reviews sections

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,8 +1,17 @@
 const CustomerReviews = () => {
   return (
-    <div>
-      CustomerReviews
-    </div>
+    <section className="max-container ">
+      <h3 className="font-palanquin text-center font-bold text-4xl">
+        What Our <span className="text-coral-red">Customers</span> Say?
+      </h3>
+      <p className="text-center text-slate-gray font-montserrat text-xl">
+        Hear genuine stories from our satisfied customers about
+        their exceptional experiences with us.
+      </p>
+      <div className="text-center items-center">
+        Morich Brown
+      </div>
+    </section>
   )
 }
 
