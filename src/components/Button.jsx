873a620715b1bdc00a47905e2c0d0891f9031619
ZwingliCaleb <calebzwingli@gmail.com>
2024-04-23 09:04:42 +0300

    creating a Nike website clone

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+//import 
+
+const button = ({label, iconURL}) => {
+  return (
+    <button className="flex justify-center font-montserrat 
+    gap-2 px-7 py-4 border items-center text-lg 
+    leading-none bg-coral-red rounded-full text-white 
+    border-coral-red">
+      {label}
+      <img
+        src={iconURL}
+        alt="Arrow right icon"
+        className="ml-2 rounded-full w-5 h-5" 
+      />
+    </button>
+  )
+}
+
+export default button
